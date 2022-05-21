50e666af1c39cd1645629fd6bc5993ad51a79bc3
Ahasan <dotnethabib@gmail.com>
2022-05-21 17:39:36 +0600

    Random Background Color Change

---
+++ app.js
@@ -0,0 +1,35 @@
+
+debugger
+ window.onload =()=>{
+     main();
+ };
+
+ function main()
+ {
+    const root = document.getElementById('root');
+    const btn  = document.querySelector('.btn')
+
+    btn.addEventListener('click', function(){
+        const rgbGenarte = GenarateRGB();
+        root.style.backgroundColor = rgbGenarte;
+       
+   
+    }
+    );
+}
+ 
+
+ function GenarateRGB() {
+ 
+     
+      const red = Math.floor(Math.random() * 255);
+     const green = Math.floor(Math.random() * 255);
+     const blue = Math.floor(Math.random() * 255);
+
+     return `rgb(${red}, ${green}, ${blue})`; 
+
+
+    
+
+ }
+ 
